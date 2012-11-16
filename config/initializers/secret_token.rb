1a6bc363122cb1114541973f84bd7e33888e2799
gitman <https://github.com/gitman>
2012-11-15 20:24:11 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '86bbfdf136fcd258eca942145aa5f8c21e482ec836cb9bdf5051fb3795369c9dc399c0305d8a43804932e0b1641bc95bd4ef0252574875dfa2a953d939cc34aa'
